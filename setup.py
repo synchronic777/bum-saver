817b3e76f6b9238a3dce92a2170c57275cadbacc
synka777 <m.barbegayet@gmail.com>
2023-12-19 14:05:30 +0100

    Changed utils files architecture and forced venv activation

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 import os
 from os import environ
+from tools.utils import log, get_dt, req_installed, get_settings
 from os.path import join, exists
-from tools.data import get_settings
-from tools.utils import s_print, req_installed
 import platform
 import shutil
 import venv
+
+
+def setup_print(step, lvl, message, *args):
+    """Standardizes the output format
+    :param step, short string that indicates to the user the step we are going through
+    :param lvl, letter that indicates if the displayed message is an Info, Warning or Error
+    :param message, the message we want to print
+    """
+    uid = None
+    log_type = 'exec'
+    if step == 'setup':
+        log_type = step
+    if step == 'uninstall':
+        log_type = step
+    if len(args) > 0:
+        uid = args[0]
+    string = f"[{(f'{uid}:' if uid else '')}{get_dt()}:{step}][{lvl}] {message}"
+    log(string, log_type)
+    print(string)
 
 
 def setup():
@@ -37,7 +55,7 @@ def setup():
         # Step 1: Copy the files into the setup folder
         # Check if the project files are all installed. If so, notify the user
         if count == len(filtered_data):
-            s_print('setup', 'I', '[1/3] OK: Project files already installed')
+            setup_print('setup', 'I', '[1/3] OK: Project files already installed')
         else:
             try:
                 if not exists(setup_folder):
@@ -48,16 +66,16 @@ def setup():
                     except IsADirectoryError:
                         shutil.copytree(f'./{elem}', f'{setup_folder}{elem}')
                     os.chmod(f'{setup_folder}{elem}', 0o500)
-                    s_print('setup', 'I', f'[1/3] OK: {setup_folder}{elem}')
-                s_print('setup', 'I', f'[1/3] OK: Copied the project into {setup_folder}')
+                    setup_print('setup', 'I', f'[1/3] OK: {setup_folder}{elem}')
+                setup_print('setup', 'I', f'[1/3] OK: Copied the project into {setup_folder}')
             except FileNotFoundError as not_found_err:
-                s_print('setup', 'E', f'[1/3]{not_found_err}')
+                setup_print('setup', 'E', f'[1/3]{not_found_err}')
 
         # Step 2: Add the alias into .bashrc/.zshrc
         shell = environ['SHELL']
         shell_string = shell.split('/')[2] if 'bash' in shell or 'zsh' in shell else None
         if not shell_string:
-            s_print('setup', 'E', '[2/3] Please use a bash or zsh shell to install the command system-wide')
+            setup_print('setup', 'E', '[2/3] Please use a bash or zsh shell to install the command system-wide')
         else:
             rc_file = f'.{shell_string}rc'
             read_rc = None
@@ -70,20 +88,20 @@ def setup():
                 if read_rc:
                     if 'shlerp' in read_rc:
                         write = False
-                        s_print('setup', 'I', '[2/3] OK: Alias function already installed')
+                        setup_print('setup', 'I', '[2/3] OK: Alias function already installed')
                 if write:
                     write_rc.write(f'source {setup_folder}config/function.template')
-                    s_print('setup', 'I', f'[2/3] OK: Alias added to {rc_file}')
+                    setup_print('setup', 'I', f'[2/3] OK: Alias added to {rc_file}')
 
         def check_deps(first_try):
             word = 'successfully'
             if not first_try:
                 word = 'already'
             if req_installed(setup_folder):
-                s_print('setup', 'I', f'[3/3] OK: Virtual environment {word} installed')
-                s_print('setup', 'I', f'✅ Install complete! Please restart your terminal to use the shlerp command')
+                setup_print('setup', 'I', f'[3/3] OK: Virtual environment {word} installed')
+                setup_print('setup', 'I', f'✅ Install complete! Please restart your terminal to use the shlerp command')
             else:
-                s_print('setup', 'E', 'ERROR: A problem happened during the requirements installation')
+                setup_print('setup', 'E', 'ERROR: A problem happened during the requirements installation')
                 exit(0)
         # Step 3: Create a venv in the setup folder and install the requirements
         venv_folder = join(setup_folder, "venv")
@@ -95,7 +113,7 @@ def setup():
             check_deps(False)
 
     else:
-        s_print('setup', 'E', 'ERROR: Shell or system not supported')
+        setup_print('setup', 'E', 'ERROR: Shell or system not supported')
         exit(0)
 
 
